172ec992e2614fa79fa8e2b530e0de24074a6ee9
JanBloch <jan@bhome.ch>
2022-03-09 14:29:21 +0100

    setup

--- index.tsx
+++ index.tsx
@@ -1,24 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
 
 interface AppProps {}
-interface AppState {
-  name: string;
-}
 
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React',
-    };
-  }
-
-  render() {
-    return <></>;
-  }
+function App({}: AppProps) {
+  return <></>;
 }
 
 render(<App />, document.getElementById('root'));
